69ee0a0d291ae911782163d8b8d3e2a855699c83
scarlet <sarahscarlet641@gmail.com>
2024-03-21 11:47:21 +0300

    create scope.js

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+  var customerName = "bob"
+    function customerName(){
+        console.log(customerName)
+        return bob
+    }
+    function upperCaseCustomerName(){
+        console.log(customerName)
+        return customerName = customerName.toUpperCase();//BOB
+    }
+    
+    function setBestCustomer() {
+        bestCustomer = 'not bob';
+    }
+    function overwriteBestCustomer() {
+        bestCustomer = 'maybe bob'
+      }
+      const leastFavoriteCustomer = "";
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'test'
+}
+
+    
+
+  
+     
